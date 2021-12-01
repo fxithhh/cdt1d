c8078efac4ce1e208c0f6d4a0786d7a2f55a849a
Seah Ying Xiang <seahyx@gmail.com>
2021-12-01 13:04:53 +0800

    Removed space

--- wordlist.py
+++ wordlist.py
@@ -3,5 +3,4 @@ easyword = ["Tiger", "Horse", "Whale","Zebra","Mouse","Moose","Camel","Koala","H
 medword = ["Bulldog","Catfish","Cheetah","Chicken","Dolphin","Giraffe","Gorilla","Hamster","Leopard","Lobster","Meerkat","Octopus","Ostrich","Peacock","Pelican","Penguin",
 "Piranha","Raccoon","Vulture","Wallaby","Sparrow","Panther","Buffalo","Seagull","Mammoth"]
 
-hardword =["Alligator","Albatross","Butterfly","Barracuda","Centipede","Chameleon","Chihuahua","Crocodile","Dalmatian","Dragonfly","Jellyfish","Millipede","Porcupine","Wolverine","Chimpanzee","Armadillo",
-"Salamander","Wildebeest","Chinchilla","Nightingale","Hummingbird","Bloodhound","Rhinoceros","Rattlesnake","Grasshopper"]
+hardword =["Alligator","Albatross","Butterfly","Barracuda","Centipede","Chameleon","Chihuahua","Crocodile","Dalmatian","Dragonfly","Jellyfish","Millipede","Porcupine","Wolverine","Chimpanzee","Armadillo","Salamander","Wildebeest","Chinchilla","Nightingale","Hummingbird","Bloodhound","Rhinoceros","Rattlesnake","Grasshopper"]
