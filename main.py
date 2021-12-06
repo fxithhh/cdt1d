ba179ed34308901ddc9dbccd7efdda0bb45eaa48
Yushenggg <94521132+Yushenggg@users.noreply.github.com>
2021-12-06 16:10:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -157,11 +157,11 @@ class CreditsFrame(gc.GameFrame):
         c_team_label.grid(row=1, column=0, columnspan=2, padx=(20, 20), pady=(10, 10), sticky='w')
 
         # Everyone and their roles
-        c_people = [('Cheng Wei Xuan', 'Gameplay Developer, Lead Artist'),
+        c_people = [('Seah Ying Xiang', 'The Avatar, Master of all 4 Languages'),
+                    ('Cheng Wei Xuan', 'Gameplay Developer, Lead Artist'),
                     ('Ang Yue Sheng', 'Gameplay Developer, Artist'),
                     ('Lim Tiang Sui, Faith', 'GUI Developer, Lead UI/UX'),
-                    ('Jeriah Yeo Ruei', 'GUI Developer, Content Lead'),
-                    ('Seah Ying Xiang', 'Noob Developer')]
+                    ('Jeriah Yeo Ruei', 'GUI Developer, Content Lead')]
 
         # Create labels from c_people
         c_people_labels = [(tk.Label(frame_text, text=name, font=root.content_font, background='white'),
